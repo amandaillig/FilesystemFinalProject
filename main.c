700c7aaa8950b644da9c789b00fafaff31ca5733
Twiztdavenger <mmuenzbe15@mail.bw.edu>
2018-11-08 19:49:13 -0500

    Initial File Upload
    Already created the main method and a function to tokenize input to find the command type.

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+void getCommandType(char command[], char * commandType) {
+    char commandToken[500];
+    size_t destination_size = sizeof (commandToken);
+
+    strncpy(commandToken, command, destination_size);
+
+    char * token;
+
+    token = strtok(commandToken, " ");
+    if(token != NULL) {
+        strcpy(commandType, token);
+    }
+}
+
+int main(int argc, char * argv[]) {
+    printf("FileSystem Final Project\n");
+    int stopLoop = 1;
+    //while(stopLoop) {
+        printf("Input command:\n");
+
+        char command[500];
+
+        fgets(command, 500, stdin);
+
+        char * commandType;
+        getCommandType(command, commandType);
+
+        if(strcmp(commandType, "create") == 0) {
+            // Create Command
+
+        } else if(strcmp(commandType, "read") == 0) {
+            // Read Command
+
+        } else if(strcmp(commandType, "delete") == 0) {
+            // Delete Command
+
+        }
+    //}
+}
