4a6a70dcae9a6c568ad529f18f099ece3e1bb2f1
Mattinator <mmuenzbe15@mail.bw.edu>
2018-11-11 21:29:57 -0500

    Made a function to get parameters from create command Started making a couple functions to get file contents input from the user, write these lines to the file in the corresponding partition, and return these filepaths to the createFileMapper function

--- main.c
+++ main.c
@@ -17,6 +17,65 @@ void getCommandType(char command[], char * commandType) {
     }
 }
 
+void getCreateParameters(char command[], int * n, char * fileName) {
+    char commandToken[500];
+    size_t destination_size = sizeof (commandToken);
+
+    strncpy(commandToken, command, destination_size);
+
+    char * token;
+    token = strtok(commandToken, " ");
+    for(int i = 0; i < 3; i ++) {
+        if(token != NULL && strlen(token) > 0) {
+            if(i == 1) {
+                strcpy(fileName, token);
+            } else if(i == 2) {
+                token[strlen(token) - 1] = 0;
+                *n = *token - '0';
+            }
+            token = strtok(NULL, " ");
+        }
+    }
+}
+
+void createFileMapping(char * filePath) {
+
+}
+
+
+char * writeToFile(int partitionCounter, char * fileName, char currentLine[]) {
+    //Return partitioned filepath
+}
+
+void inputFileContents(int n, char * fileName) {
+    int endOfFileContents = 0;
+    int partitionCounter = 1;
+    int lineNo = 1;
+    char* partitionFilePaths[n];
+
+    while(!endOfFileContents) {
+
+        if(partitionCounter > n) {
+            partitionCounter = 1;
+        }
+
+        char currentLine[500];
+        printf("Enter the contents of the file, terminated by an empty line:\n");
+        printf("%d: ", lineNo);
+        fgets(currentLine, 500, stdin);
+
+        if(strcmp(currentLine, "\n") == 0) {
+            endOfFileContents = 1;
+        } else {
+            partitionFilePaths[partitionCounter - 1] = writeToFile(partitionCounter, fileName, currentLine);
+            lineNo++;
+            partitionCounter++;
+        }
+    }
+    createFileMapping(partitionFilePaths);
+}
+
+
 int main(int argc, char * argv[]) {
     printf("FileSystem Final Project\n");
     int stopLoop = 1;
@@ -32,6 +91,13 @@ int main(int argc, char * argv[]) {
 
         if(strcmp(commandType, "create") == 0) {
             // Create Command
+
+            int noOfPartitions = 0;
+            char * fileName = (char*)malloc(100);
+
+            getCreateParameters(command, &noOfPartitions, fileName);
+
+            inputFileContents(noOfPartitions, fileName);
 
         } else if(strcmp(commandType, "read") == 0) {
             // Read Command
